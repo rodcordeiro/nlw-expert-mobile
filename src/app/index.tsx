d05773245ca7890603823699ca7174697b3f89b1
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-02-06 18:32:32 -0300

    feat: cart indicator at header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { Header } from '@/components/header';
 export default function Home() {
   return (
     <View className="flex-1 pt-10">
-      <Header title="Faça seu pedido" />
+      <Header title="Faça seu pedido" cartQuantityItems={10} />
     </View>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,30 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import colors from 'tailwindcss/colors';
 
 type HeaderProps = {
   title: string;
+  cartQuantityItems?: number;
 };
 
-export function Header({ title }: HeaderProps) {
+export function Header({ title, cartQuantityItems }: HeaderProps) {
   return (
     <View className="flex-row items-center border-b border-slate-700 pb-5 mx-5">
       <View className="flex-1">
         <Image source={require('@/assets/logo.png')} className="h-6 w-32" />
         <Text className="text-white text-xl font-heading mt-2">{title}</Text>
       </View>
+
+      {cartQuantityItems && (
+        <TouchableOpacity className="relative">
+          <View className="bg-lime-300 w-5 h-5 rounded-full items-center justify-center top-2 z-10 -right-3.5">
+            <Text className="text-slate-900 font-bold text-xs">
+              {cartQuantityItems}
+            </Text>
+          </View>
+          <Feather name="shopping-bag" color={colors.white} size={24} />
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
